06307252f7846dffd90f352fa19eabe346e23e90
Amarendar M <musham.amarendar@gmail.com>
2017-11-29 14:23:11 +0530

    create virtual box

---
+++ virtualboxinstall.sh
@@ -0,0 +1,4 @@
+sudo apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install virtualbox-5.0 dkms 
