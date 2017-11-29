9ab619151d1444a71f41a7d8f9086b3e4d531bf4
devs@gmial.com <devops@gmail.com>
2017-11-29 11:39:27 +0530

    maven instal and path addition

---
+++ maven_installzipfile.sh
@@ -0,0 +1,10 @@
+cd ~
+wget "http://redrockdigimark.com/apachemirror/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip"
+unzip apache-maven-3.5.2-bin.zip
+rm apache-maven-3.5.2-bin.zip
+`export PATH=$PATH:~/apache-maven-3.5.2/bin`
+echo 
+echo 
+echo "export PATH=$PATH:~/apache-maven-3.5.2/bin \n if this isn't working then add it to ur bashrc file in ~/.bashrc"
+echo 
+echo 
